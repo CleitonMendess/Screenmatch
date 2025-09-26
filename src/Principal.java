63cd78625a0030de6e64e1ca8c8028c7aa139335
sinik6 <vandilsonferreirajr@gmail.com>
2025-09-25 23:09:21 -0300

    Update Principal.java corrigindo pra adequar com Filme.java
    atualizando com as aulas

--- src/Principal.java
+++ src/Principal.java
@@ -1,23 +1,23 @@
-import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.Filme;
 import javax.swing.JOptionPane;
-
+ 
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
-        meuFilme.setNome("O poderoso chefão");
-        meuFilme.setAnoDeLancamento(1970);
-        meuFilme.setDuracaoEmMinutos(180);
-        meuFilme.incluidoNoPlano = true;
+        meuFilme.nome = "O poderoso chefão";
+        meuFilme.setAnoDeLancamento(1970); 
+        meuFilme.setDuracaoEmMinutos(180); 
+
+        meuFilme.exibeFichaTecnica();
+
+        String mensagem = "Nome: " + meuFilme.nome + "\n";
+        mensagem += "Ano de lançamento: " + meuFilme.anoDeLancamento + "\n";
+        mensagem += "Duração: " + meuFilme.getDuracaoEmMinutos() + " minutos\n";
 
         meuFilme.avalia(8);
         meuFilme.avalia(5);
         meuFilme.avalia(10);
-
-        //deixando menor o mesmo codigo
-        String mensagem = "Nome: " + meuFilme.nome + "\n";
-        mensagem += "Ano de lançamento: " + meuFilme.anoDeLancamento + "\n";
-        mensagem += "Duração: " + meuFilme.getDuracaoEmMinutos() + " minutos\n";
-        
-       JOptionPane.showMessageDialog(null, mensagem, "Ficha Técnica do Filme", JOptionPane.INFORMATION_MESSAGE);
+      System.out.println(meuFilme.pegaMedia());
+      JOptionPane.showMessageDialog(null, mensagem, "Ficha Técnica do Filme", JOptionPane.INFORMATION_MESSAGE);
     }
 }
